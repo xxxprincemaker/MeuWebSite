f4a61a4498ffcecf7476ca4a2888a6276a9a077e
Prince <luizfas@dcc.ufrj.br>
2022-10-29 14:18:17 -0300

    bla bla bla

--- othello/app.js
+++ othello/app.js
@@ -1,3 +1,5 @@
+let jogador = 1;
+
 (function teste() {
   console.log("teste");
 })();
@@ -8,43 +10,13 @@ const matrixCanvas = Array(8)
   .fill()
   .map(() => Array(8).fill(CoresPecas.Vazia));
 
-function criarTabuleiro() {
+(function criarTabuleiro() {
   matrixCanvas[3][3] = CoresPecas.Branca;
   matrixCanvas[3][4] = CoresPecas.Preta;
   matrixCanvas[4][4] = CoresPecas.Branca;
   matrixCanvas[4][3] = CoresPecas.Preta;
-}
-
-criarTabuleiro();
-/*
-(function criarLinhasHorizontais() {
-  let otheloCanvas = document.getElementById("othelloCanvas");
-  let ctx = otheloCanvas.getContext("2d");
-  for (let i = 1; i <= 8; i++) {
-    ctx.moveTo(0, 100 * i);
-    ctx.lineTo(800, 100 * i);
-    ctx.strokeStyle = "#8A2BE2";
-    ctx.stroke();
-  }
 })();
 
-(function criarLinhasVerticais() {
-  let otheloCanvas = document.getElementById("othelloCanvas");
-  let ctx = otheloCanvas.getContext("2d");
-  ctx.moveTo(0, 0);
-  for (let i = 1; i <= 8; i++) {
-    ctx.moveTo(100 * i, 0);
-    ctx.lineTo(100 * i, 800);
-    ctx.strokeStyle = "#8A2BE2";
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc((50)*i, 50, 40, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'white';
-    ctx.stroke();
-  }
-})();
-*/
 
 const canvas = document.getElementById("canvasOthello");
 (function desenharTabuleiro() {
@@ -96,19 +68,19 @@ const canvas = document.getElementById("canvasOthello");
         for(let j = 1; j <= 8; j++){
         let element = document.getElementById('bloco['+i+','+j+']');
         element.addEventListener('click', function(){
-            if(matrixCanvas[i-1][j-1] == CoresPecas.Vazia){
-                matrixCanvas[i-1][j-1] = CoresPecas.Branca;
-                document.getElementById('circulo['+i+','+j+']').style.backgroundColor = 'white';
-                document.getElementById('circulo['+i+','+j+']').style.border = '1px solid #8A2BE2';
-                document.getElementById('circulo['+i+','+j+']').style.display = 'flex';
-                }
-            else if(matrixCanvas[i-1][j-1] == CoresPecas.Branca){
-                matrixCanvas[i-1][j-1] = CoresPecas.Vazia;
-                document.getElementById('circulo['+i+','+j+']').style.display = 'none';
-                }
-            else if(matrixCanvas[i-1][j-1] == CoresPecas.Preta){
-                matrixCanvas[i-1][j-1] = CoresPecas.Branca;
-                document.getElementById('circulo['+i+','+j+']').style.backgroundColor = 'white';
+            if(jogador == 1){
+                    matrixCanvas[i-1][j-1] = CoresPecas.Branca;
+                    document.getElementById('circulo['+i+','+j+']').style.backgroundColor = 'white';
+                    document.getElementById('circulo['+i+','+j+']').style.border = '1px solid white';
+                    document.getElementById('circulo['+i+','+j+']').style.display = 'flex';
+                    jogador = 2;
+            }
+            else {
+                    matrixCanvas[i-1][j-1] = CoresPecas.Preta;
+                    document.getElementById('circulo['+i+','+j+']').style.display = 'flex';
+                    document.getElementById('circulo['+i+','+j+']').style.border = '1px solid #8A2BE2';
+                    document.getElementById('circulo['+i+','+j+']').style.backgroundColor = '#8A2BE2';
+                    jogador = 1;
                 }
             });
         }
